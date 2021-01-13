cf67417c5ad44fd48eea259c335f584ba43246c8
french-paragon <french.paragon@gmail.com>
2021-01-13 09:34:27 +0800

    Add view extraction.
    A view is a multidim array represinting a view to a subpart of the main
    array. Dimension can be sclided (they are present in the viewed array)
    or indexed (only one index is selected for the given dimension, the
    dimension in not present in the viewed array).

--- include/MultidimArrays.h
+++ include/MultidimArrays.h
@@ -19,6 +19,8 @@ limitations under the License.
 
 #include <array>
 #include <stdexcept>
+#include <cstring>
+#include <tuple>
 #include <assert.h>
 
 namespace Multidim {
@@ -35,16 +37,6 @@ constexpr bool allDimsValid() {
 	return allDimsValid<d>() and allDimsValid<ds...>();
 }
 
-template<size_t S>
-bool allDimsValid(std::array<array_size_t, S> const& array) {
-	for (size_t i = 0; i < S; i++) {
-		if (!allDimsValid(array[i])) {
-			return false;
-		}
-	}
-	return true;
-}
-
 template<typename ... Ds>
 bool allDimsValid(array_size_t d, Ds... dims) {
 	return allDimsValid(d) and allDimsValid(dims...);
@@ -55,10 +47,76 @@ inline bool allDimsValid(array_size_t d) {
 	return d > 0;
 }
 
+template<size_t S>
+bool allDimsValid(std::array<array_size_t, S> const& array) {
+	for (size_t i = 0; i < S; i++) {
+		if (!allDimsValid(array[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
 enum class AccessCheck {
 	Check = 1,
 	Nocheck = 0
 };
+
+struct DimInfo {
+	virtual bool stayDim() const = 0;
+};
+
+struct DimSlice : public DimInfo {
+
+	DimSlice(array_size_t sIndex = 0,
+			 array_size_t eIndex = -1,
+			 array_size_t jump = 1) :
+		startIndex(sIndex),
+		endIndex(eIndex),
+		indexJump(jump)
+	{
+
+	}
+
+	array_size_t startIndex;
+	array_size_t endIndex;
+	array_size_t indexJump;
+
+	virtual bool stayDim() const override {
+		return true;
+	}
+};
+
+struct DimIndex : public DimInfo {
+
+	DimIndex(array_size_t id) :
+		index(id)
+	{
+
+	}
+
+	array_size_t index;
+
+	virtual bool stayDim() const override {
+		return false;
+	}
+};
+
+
+template<typename s, typename... slices>
+constexpr array_size_t slicedDims() {
+	return slicedDims<s>() + slicedDims<slices...>();
+}
+
+template<>
+constexpr array_size_t slicedDims<DimSlice>() {
+	return 1;
+}
+
+template<>
+constexpr array_size_t slicedDims<DimIndex>() {
+	return 0;
+}
 
 template<typename T, array_size_t nDim>
 class Array {
@@ -71,7 +129,7 @@ public:
 	typedef std::array<array_size_t, nDim> ShapeBlock;
 
 	class IndexBlock : public ShapeBlock {
-
+	public:
 		IndexBlock& operator+=(array_size_t scalar) {
 			for (int i = 0; i < nDim; i++) {
 				(*this)[i] += scalar;
@@ -268,16 +326,118 @@ public:
 		return _manageData;
 	}
 
-	template<AccessCheck c = AccessCheck::Check, typename... Ds>
-	int flatIndex(Ds... idxs) const {
+private:
 
-		static_assert(sizeof...(idxs) == nDim,
-				"Wrong number of indices provided");
-
-		ShapeBlock idxs_array = {idxs...};
-		return flatIndex<c>(idxs_array);
-
+	array_size_t sliceFirstIndex(DimSlice const& slice) {
+		return slice.startIndex;
 	}
+
+	array_size_t sliceFirstIndex(DimIndex const& index) {
+		return index.index;
+	}
+
+	template<typename... slices>
+	ShapeBlock firstIndex(slices... s) {
+		return {sliceFirstIndex(s)...};
+	}
+
+	template<typename... slices>
+	int dataOffset(slices... s) {
+		return flatIndex(firstIndex(s...));
+	}
+
+	template<typename... slices>
+	typename Array<T, slicedDims<slices...>()>::ShapeBlock getSubShape(slices... s) {
+
+		using SBlock = typename Array<T, slicedDims<slices...>()>::ShapeBlock;
+
+		std::array<const DimInfo *, sizeof... (slices)> a = {static_cast<const DimInfo *>(&s)...};
+		SBlock r;
+
+		int p = 0;
+		for (int i = 0; i < sizeof... (slices); i++) {
+			if (a[i]->stayDim()) {
+				const DimSlice * s = static_cast<const DimSlice *>(a[i]);
+
+				array_size_t eId = (s->endIndex >= 0) ? s->endIndex : (_shape[i] - s->endIndex + 1);
+				array_size_t sId = (s->startIndex >= 0) ? s->startIndex : (_shape[i] - s->startIndex + 1);
+				array_size_t step = s->indexJump;
+
+				if (sId < 0 or sId >= _shape[i]) {
+					throw std::out_of_range("Start index out of range");
+				}
+
+				if (eId < 0 or eId >= _shape[i]) {
+					throw std::out_of_range("End index out of range");
+				}
+
+				if (sId == eId) {
+					throw std::out_of_range("Slice lead to an empty range");
+				}
+
+				if (sId > eId) {
+
+					if (step >= 0) {
+						throw std::out_of_range("Step is invalid");
+					}
+
+					array_size_t tmp = eId;
+					eId = sId;
+					sId = tmp;
+
+					step = -step;
+
+				} else if (step <= 0) {
+					throw std::out_of_range("Step is invalid");
+				}
+
+				array_size_t range = std::abs(eId - sId);
+
+				r[p] = 1 + (range-1)/step;
+				p++;
+			}
+		}
+
+		return r;
+	}
+
+	template<typename... slices>
+	typename Array<T, slicedDims<slices...>()>::ShapeBlock getSubStrides(slices... s) {
+
+		using SBlock = typename Array<T, slicedDims<slices...>()>::ShapeBlock;
+
+		std::array<DimInfo const*, sizeof... (slices)> a = {static_cast<DimInfo const*>(&s)...};
+		SBlock r;
+
+		int p = 0;
+		for (int i = 0; i < sizeof... (slices); i++) {
+			if (a[i]->stayDim()) {
+				r[p] = _strides[i];
+				p++;
+			}
+		}
+
+		return r;
+	}
+
+public:
+
+	template<typename... slices>
+	Array<T, slicedDims<slices...>()> subView(slices... s) {
+
+		static_assert (sizeof... (slices) == nDim, "Cannot generate an array with zero dimensions");
+		static_assert (slicedDims<slices...>() > 0, "Cannot generate an array with zero dimensions");
+
+		using SubArray = Array<T, slicedDims<slices...>()>;
+		using SubShapeBlock = typename SubArray::ShapeBlock;
+
+		int offset = dataOffset(s...);
+		SubShapeBlock shape = getSubShape(s...);
+		SubShapeBlock strides = getSubStrides(s...);
+
+		return SubArray(_data + offset, shape, strides);
+	}
+
 
 	template<AccessCheck c = AccessCheck::Check>
 	int flatIndex(ShapeBlock const& idxs) const {
@@ -300,17 +460,48 @@ public:
 
 	}
 
+	template<AccessCheck c = AccessCheck::Check, typename... Ds>
+	int flatIndex(int idx0, Ds... idxs) const {
 
-	template<AccessCheck c = AccessCheck::Check>
-	int flatIndex(int index) {
+		static_assert(sizeof...(idxs) == nDim -1 or sizeof...(idxs) == 0,
+				"Wrong number of indices provided");
 
-		if (c == AccessCheck::Check) {
-			if (index < 0 or index >= flatLenght()) {
-				throw std::out_of_range("Index out of range");
+		if (sizeof...(idxs) == 0) {
+
+			if (c == AccessCheck::Check) {
+				if (idx0 < 0 or idx0 >= flatLenght()) {
+					throw std::out_of_range("Index out of range");
+				}
 			}
+
+			return idx0;
+
+		} else {
+
+			ShapeBlock idxs_array = {idx0, idxs...};
+			return flatIndex<c>(idxs_array);
+
 		}
 
-		return index;
+	}
+
+	bool copyData(Array<T,nDim> const& other) {
+
+		if (other.shape() != shape()) {
+			return false;
+		}
+
+		int n = flatLenght();
+
+		const T* ptr = other._data;
+		if (ptr >= _data and ptr < _data + n) {
+			return false;
+		}
+
+		memcpy(_data, ptr, n*sizeof (T));
+
+		return true;
+
 	}
 
 	template<AccessCheck c = AccessCheck::Check, typename... Ds>
@@ -325,15 +516,37 @@ public:
 		return _data[fIds];
 	}
 
+	template<typename... Ds>
+	T& atUnchecked(Ds... idxs) {
+		int fIds = flatIndex<AccessCheck::Nocheck>(idxs...);
+		return _data[fIds];
+	}
+
+	T& atUnchecked(ShapeBlock const& idxs) {
+		int fIds = flatIndex<AccessCheck::Nocheck>(idxs);
+		return _data[fIds];
+	}
+
 	template<AccessCheck c = AccessCheck::Check, typename... Ds>
 	T value(Ds... idxs) const {
 		int fIds = flatIndex<c>(idxs...);
 		return _data[fIds];
 	}
 
-	template<AccessCheck c = AccessCheck::Check, typename... Ds>
+	template<AccessCheck c = AccessCheck::Check>
 	T value(ShapeBlock const& idxs) const {
 		int fIds = flatIndex<c>(idxs);
+		return _data[fIds];
+	}
+
+	template<typename... Ds>
+	T valueUnchecked(Ds... idxs) const {
+		int fIds = flatIndex<AccessCheck::Nocheck>(idxs...);
+		return _data[fIds];
+	}
+
+	T valueUnchecked(ShapeBlock const& idxs) const {
+		int fIds = flatIndex<AccessCheck::Nocheck>(idxs);
 		return _data[fIds];
 	}
 	
